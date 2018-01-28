7db5524f7fbcdfd1278956d4252c899991bfe506
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2018-01-28 18:20:54 +0100

    added switch  to routes

--- src/components/rentals/rental-create/RentalCreateForm.js
+++ src/components/rentals/rental-create/RentalCreateForm.js
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import {OgaInput}  from '../../shared/form/OgaInput';
+import {OgaTextBox}  from '../../shared/form/OgaTextBox';
 import {OgaError} from '../../shared/form/OgaError';
+import {required,minLength30, Isnumber} from '../../shared/form/validate'
+import {OgaSelect} from '../../shared/form/OgaSelect';
+import {OgaFileUpload} from '../../shared/form/OgaFileUpload';
+
 import {reduxForm,Field} from 'redux-form'
-import {required, maxLength40} from '../../shared/form/validate'
+
 
 const RentalCreateForm  = props=> {
-  const { errors, Submit,handleSubmit ,valid, pristine,  submitting} = props
+  const { errors, Submit,handleSubmit ,valid, pristine,  submitting, options} = props
   return (
    <form onSubmit={handleSubmit(Submit)}>
       <Field
@@ -14,8 +20,17 @@ const RentalCreateForm  = props=> {
        type="text"
        label="Title"
        className="form-control"
-       placeholder ="title"
        validate = {[required]}
+      />
+      <Field
+       name="description"
+       component = {OgaTextBox}
+       type="text"
+       label="Description"
+       column = {6}
+       row= {3}
+       className="form-control"
+       validate = {[required,minLength30]}
       />
        <Field
        name="city"
@@ -23,7 +38,6 @@ const RentalCreateForm  = props=> {
        type="text"
        label="City"
        className="form-control"
-       placeholder ='city try  "ABUJA" '
        validate ={[required]}
       />
       <Field
@@ -32,28 +46,39 @@ const RentalCreateForm  = props=> {
        type="text"
        label="Street"
        className="form-control"
-       placeholder ='Street'
+       validate ={[required]}
+      />
+      <Field
+       name="category"
+       component={OgaSelect}
+       options = {options}
+       type="text"
+       label="Category"
+       className="form-control"
        validate ={[required]}
       />
        <Field
-       name="BedRoom"
-       component={OgaInput}
-       type="number"
-       label="BedRoom"
-       className="form-control"
-       placeholder ='Street'
-       validate ={[required]}
-      />
-       <Field
-       name="dailyRate"
-       component={OgaInput}
-       type="number"
-       label="Price per day"
-       className="form-control"
-       placeholder ='Price per day'
-       validate ={[required]}
+       name="Image"
+       component={OgaFileUpload}
+       label="Image"
       />
 
+    <Field
+       name="Bedrooms"
+       component={OgaInput}
+       type="text"
+       label="Bedrooms"
+       className="form-control"
+       validate ={[required, Isnumber]}
+      />
+      <Field
+       name="DailyRate"
+       component={OgaInput}
+       type="text"
+       label="DailyRate"
+       className="form-control"
+       validate ={[required, Isnumber]}
+      />
      <button type="submit" disabled={ !valid || pristine || submitting}   className="btn btn-bwm bwm-form">
           Create Rental
     </button>
@@ -65,5 +90,5 @@ const RentalCreateForm  = props=> {
 }
 
 export default reduxForm({
-    form: 'RentalCreateForm'
+    form: 'rentalCreateForm'
   })(RentalCreateForm)
