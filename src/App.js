f9ee720f027113aa526cbd8729a8250d5370349d
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2018-01-28 18:11:41 +0100

    did some little refoctorimg for app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import Header from './components/shared/Header';
 import { BrowserRouter,Route,Redirect } from "react-router-dom";
+import {Provider} from 'react-redux';
+
+import Header from './components/shared/Header';
 import RentalList from './components/rentals/rental-list/RentalListing';
 import RentalDetail from "./components/rentals/rental-detail/RentalDetail";
-import {Provider} from 'react-redux';
 import Register from "./components/register/Register";
 import RentalCreate from "./components/rentals/rental-create/RentalCreate";
 import Login from './components/login/Login'
 import  {ProtectedRoute} from './components/shared/auth/ProtectedRoute'
 import RentalSearchListing from './components/rentals/rental-list/RentalSearchListing'
+
+
 import * as actions from '../src/actions/index'
 
 
