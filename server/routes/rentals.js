9b92c41f730708eaa4d48e18094a1f3f911d4ef5
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2018-04-01 09:41:59 +0100

    edited it

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -5,26 +5,20 @@ const {normalizeError} = require("../helpers/mongoose")
 
 router.get("", function(req,res){
   const {city} = req.query
-  if (city) {
-      Rental.find({city : city.toLowerCase()})
-      .select("-booking")
-      .exec((err, fillterdRental)=>{
-          if (err) {
-              return   res.status(422).send({errors :normalizeError(err.errors) })   
-          }
-          if (fillterdRental.length === 0 ) {
-            return   res.status(422).send({errors :[{title:"Rental error", detail : `Could not find a rental for city ${city}`}]})   
-          } 
-          return  res.json(fillterdRental)
-      })
-  } else {
-    Rental.find({})
+  const query =  city  ? {city : city.toLowerCase()} :  {}
+
+    Rental.find(query)
     .select("-bookings")
     .exec((err,FoundRental)=>{
-         return    res.json(FoundRental)
+      if (err) {
+        return   res.status(422).send({errors :normalizeError(err.errors) })   
+      }
+      if ( city && FoundRental.length === 0 ) {
+        return   res.status(422).send({errors :[{title:"Rental error", detail : `Could not find a rental for city ${city}`}]})   
+      } 
+        return    res.json(FoundRental)
     }) 
-  }
-})
+  })
 
 
 router.get("/:id", function(req,res){
