06f8c9eb64c3edd4f5357116b2fe18e60898e5a4
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2018-04-01 09:30:22 +0100

    edited my fakedb storege location

--- server/fake-db.js
+++ server/fake-db.js
@@ -1,52 +1,11 @@
 const Rental = require("./models/rental")
 const User = require('./models/users')
+const Data =  require('./data.json')
 
 class FakeDB {
     constructor() {
-        this.rentals = [{
-            title: "Nice view on ocean",
-            city: "San Francisco",
-            street: "Main street",
-            category: "condo",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 4,
-            shared: true,
-            description: "the bes t rntalthe city.",
-            dailyRate: 43
-            },
-            {
-            title: "Modern apartment in center",
-            city: "New York",
-            street: "Time Square",
-            category: "apartment",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 1,
-            shared: false,
-            description: "Very nice apartment in center of the city.",
-            dailyRate: 11
-            },
-            {
-            title: "Old house in nature",
-            city: "Spisska Nova Ves",
-            street: "Banicka 1",
-            category: "house",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 5,
-            shared: true,
-            description: "Very nice apartment in center of the city.",
-            dailyRate: 23
-        }]
-        this.users = [{
-            username : "test test",
-            email : "test@gmail.com",
-            password : "testtest"
-        },
-        {
-          username : "test test1",
-          email : "test1@gmail.com",
-          password : "testtest1"
-      },
-      ]
+        this.rentals =Data.rentals
+        this.users =  Data.user
     }
     async CleanDb(){
       await  User.deleteMany({})
