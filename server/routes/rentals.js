0bd1ae3492edc5a1e0d44632fe78220eb99b0ce9
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2018-04-01 09:56:38 +0100

    started working on create rental end point

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {findRentals,findRentalByid} =require('../controllers/rental')
+const {findRentals,findRentalByid,createRental} =require('../controllers/rental')
 
 router.get("", findRentals)
 
 router.get("/:id", findRentalByid)
 
+router.post("", createRental)
 
 module.exports = router
