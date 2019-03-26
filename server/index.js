bb1959442d18a0ef0b70ade909173e2615f1414f
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-03-26 03:04:06 +0100

    create api to  fecth all rental and rental by id

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const config = require("./config/dev")
-const app = express();
-const Rental = require("./models/rental")
 const FakeDB = require("./fake-db")
+const Rentalroute = require("./routes/rentals")
 
 mongoose.connect(config.DB_URL).then(()=>{
     const fakedb = new FakeDB();
     fakedb.seedDb()
-}
-)
+})
+ 
+const app = express();
+
+app.use("/api/v1/rentals",Rentalroute)
 
 var PORT = process.env.PORT || 3001
-
-app.get("/test",(req,res)=>{
-   res.json({"test":"true"})
-})
 
 app.listen(PORT,function(){
     console.log("am runin on"+PORT)
