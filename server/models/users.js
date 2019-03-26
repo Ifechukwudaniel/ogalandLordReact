b5387ea4a3e8810be5df409485f39c780bbffe81
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-03-26 23:13:40 +0100

    started creating user model

--- server/models/users.js
+++ server/models/users.js
@@ -4,5 +4,5 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
     username :{type:String, min:[4, 'To short for an email string' ], max:[32,'Too long for a username'],required :true,},
     email :{type:String,  min:[4,'To short for an email string'],   max:[32,'To short for an email string'],lowercase:true,
-    unique:true,match:"/^\w+([\.-]?\w+)*@\w+([\.-]?\w+) "}
+    unique:true,match:"/^\w+([\.-]?\w+)*@\w+([\.-]?\w+) wswdw"}
 })
