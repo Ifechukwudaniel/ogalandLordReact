a4d0c9f7a25811908baeb844c566c7dea0af3e03
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-03-29 01:38:30 +0100

    added the login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+  render() {
+      console.log(this.state)
+    return (
+      <div>
+         <h1>i am a  login componet</h1>
+      </div>
+    )
+  }
+}
+
+export default Login
